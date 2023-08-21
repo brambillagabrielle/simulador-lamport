ef12bdcc7c727ca3eb77c479190be08bef86c226
Gabrielle Brambilla <69489731+brambillagabrielle@users.noreply.github.com>
2023-08-21 13:49:01 -0300

    simulador lamport

--- simuladorlamport.go
+++ simuladorlamport.go
@@ -10,80 +10,86 @@ func main() {
 
 	rand.Seed(time.Now().UnixNano())
 
-	quantProcessos := 2
+	var quantProcessos int
 
 	fmt.Print("Insira o número de processos desejados: ")
 	fmt.Scan(&quantProcessos)
-
-	pN := criaMatriz(quantProcessos, 10)
-	contadorFrequenciaProcessos(pN, quantProcessos)
-
-	eventos := criaMatriz(4, 1)
-
-	var acao string
-	for {
-
-		mostrarMatrizProcessos(pN, quantProcessos)
-
-		fmt.Println()
-		fmt.Println(" * Novo evento *")
-		var processoOrigem, tempoOrigem, processoDestino, tempoDestino int
-
-		fmt.Print("Processo de origem: ")
-		fmt.Scan(&processoOrigem)
-
-		if processoOrigem >= 0 && processoOrigem <= quantProcessos {
-
-			fmt.Print("Tempo de origem: ")
-			fmt.Scan(&tempoOrigem)
-
-			if tempoOrigem >= 0 && tempoOrigem <= (tempoOrigem*9) {
-
-				fmt.Print("Processo de destino: ")
-				fmt.Scan(&processoDestino)
-
-				if processoDestino >= 0 && processoDestino <= quantProcessos {
-
-					fmt.Print("Tempo de destino: ")
-					fmt.Scan(&tempoDestino)
-
-					if tempoDestino >= 0 && tempoDestino <= (tempoDestino*9) {
-
-						novoTempoDestino := 0
-
-						if tempoOrigem < tempoDestino {
-							novoTempoDestino = 0
-						} else {
-							novoTempoDestino = ajustaContadorProcesso(pN, tempoOrigem, processoDestino, tempoDestino)
-						}
-
-						evento := []int{processoOrigem, tempoOrigem, processoDestino, tempoDestino, novoTempoDestino}
-						eventos = append(eventos, evento)
-						mostrarUltimoEvento(eventos)
-
-					} else {
-						fmt.Println("Erro na entrada do tempo de destino")
-					}
-
-				} else {
-					fmt.Println("Erro na entrada do processo de destino")
-				}
-
-			} else {
-				fmt.Println("Erro na entrada do tempo de origem")
-			}
-
-		} else {
-			fmt.Println("Erro na entrada do processo de origem")
-		}
-
-		fmt.Print("Deseja sair? (S/N): ")
-		fmt.Scan(&acao)
-
-		if acao == "S" {
-			break
-		}
-
+	
+	if (quantProcessos >= 2) {
+	    
+	    pN := criaMatriz(quantProcessos, 10)
+    	contadorFrequenciaProcessos(pN, quantProcessos)
+    
+    	eventos := criaMatriz(4, 1)
+    
+    	var acao string
+    	for {
+    
+    		mostrarMatrizProcessos(pN, quantProcessos)
+    
+    		fmt.Println()
+    		fmt.Println(" * Novo evento *")
+    		var processoOrigem, tempoOrigem, processoDestino, tempoDestino int
+    
+    		fmt.Print("Processo de origem: ")
+    		fmt.Scan(&processoOrigem)
+    
+    		if processoOrigem >= 0 && processoOrigem <= quantProcessos {
+    
+    			fmt.Print("Tempo de origem: ")
+    			fmt.Scan(&tempoOrigem)
+    
+    			if tempoOrigem >= 0 && tempoOrigem <= (tempoOrigem*9) && tempoOrigem % (pN[1][processoOrigem] - pN[0][processoOrigem]) == 0 {
+    
+    				fmt.Print("Processo de destino: ")
+    				fmt.Scan(&processoDestino)
+    
+    				if processoDestino >= 0 && processoDestino <= quantProcessos {
+    
+    					fmt.Print("Tempo de destino: ")
+    					fmt.Scan(&tempoDestino)
+    
+    					if tempoDestino >= 0 && tempoDestino <= (tempoDestino*9) && tempoDestino % (pN[1][processoDestino] - pN[0][processoDestino]) == 0 {
+    
+    						novoTempoDestino := 0
+    
+    						if tempoOrigem < tempoDestino {
+    							novoTempoDestino = 0
+    						} else {
+    							novoTempoDestino = ajustaContadorProcesso(pN, tempoOrigem, processoDestino, tempoDestino)
+    						}
+    
+    						evento := []int{processoOrigem, tempoOrigem, processoDestino, tempoDestino, novoTempoDestino}
+    						eventos = append(eventos, evento)
+    						mostrarUltimoEvento(eventos)
+    
+    					} else {
+    				        fmt.Println("Tempo de destino ", tempoDestino, " é inválido para o processo ", processoDestino, "!")
+    					}
+    
+    				} else {
+    			        fmt.Println("O ID ", processoDestino, " é inválido!")
+    				}
+    
+    			} else {
+    				fmt.Println("Tempo de origem ", tempoOrigem, " é inválido para o processo ", processoOrigem, "!")
+    			}
+    
+    		} else {
+    			    fmt.Println("O ID ", processoOrigem, " é inválido!")
+    		}
+    
+    		fmt.Print("Deseja sair? (S/N): ")
+    		fmt.Scan(&acao)
+    
+    		if acao == "S" || acao == "s" {
+    			break
+    		}
+    
+    	}
+	    
+	} else {
+	    fmt.Println("Erro na entrada da quantidade de processos! Programa encerrado.")
 	}
 
 }
@@ -161,15 +167,14 @@ func ajustaContadorProcesso(pN [][]int, tempoOrigem, processoDestino, tempoDesti
 
 }
 
+// Mostra 
 func mostrarUltimoEvento(eventos [][]int) {
 
 	ultimoEvento := len(eventos) - 1
 
 	fmt.Println()
-	fmt.Print("O processo ", eventos[ultimoEvento][0])
-	fmt.Print(" enviou no tempo ", eventos[ultimoEvento][1])
-	fmt.Print(" para o processo ", eventos[ultimoEvento][2])
-	fmt.Print(" que recebeu no tempo ", eventos[ultimoEvento][3])
+	fmt.Print("O processo ", eventos[ultimoEvento][0], " enviou no tempo ", eventos[ultimoEvento][1])
+	fmt.Print(" para o processo ", eventos[ultimoEvento][2], " que recebeu no tempo ", eventos[ultimoEvento][3])
 	fmt.Println()
 
 	if eventos[ultimoEvento][4] != 0 {
